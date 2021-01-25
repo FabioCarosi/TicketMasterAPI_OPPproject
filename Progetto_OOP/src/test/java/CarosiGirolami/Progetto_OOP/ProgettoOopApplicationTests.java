a78309b74c8cfef871452bab4f4060b1794d2535
Fabio Carosi <s1089298@studenti.univpm.it>
2021-01-25 18:47:58 +0100

    Prova

---
+++ Progetto_OOP/src/test/java/CarosiGirolami/Progetto_OOP/ProgettoOopApplicationTests.java
@@ -0,0 +1,13 @@
+package CarosiGirolami.Progetto_OOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
