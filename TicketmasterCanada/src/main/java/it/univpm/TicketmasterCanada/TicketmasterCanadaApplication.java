54d2971d58303aa51fd3788cca6856d29f452729
Fabio Carosi <s1089298@studenti.univpm.it>
2021-01-26 17:16:24 +0100

    v. 0.0.0.1

---
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/TicketmasterCanadaApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.TicketmasterCanada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmasterCanadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmasterCanadaApplication.class, args);
+	}
+
+}
---
+++ TicketmasterCanada/src/test/java/it/univpm/TicketmasterCanada/TicketmasterCanadaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.TicketmasterCanada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterCanadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
