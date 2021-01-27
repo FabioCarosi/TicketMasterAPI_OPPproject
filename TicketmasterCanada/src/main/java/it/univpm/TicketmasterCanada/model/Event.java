8ba3c99e882cc2c852a9e7af491842d468a9b2b0
Fabio Carosi <s1089298@studenti.univpm.it>
2021-01-27 15:43:30 +0100

    v. 0.0.1.0

--- TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/Event.java
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/Event.java
@@ -13,13 +13,74 @@ public class Event {
 	private String name;
 	private String url;
 	private String info;
+	/**
+	 * 
+	 */
+	public Event() {
+		super();
+	}
+	/**
+	 * @param id
+	 * @param name
+	 * @param url
+	 * @param info
+	 */
+	public Event(String id, String name, String url, String info) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.url = url;
+		this.info = info;
+	}
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	/**
+	 * @return the info
+	 */
+	public String getInfo() {
+		return info;
+	}
+	/**
+	 * @param info the info to set
+	 */
+	public void setInfo(String info) {
+		this.info = info;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
+		
 	
 }
