f0ac2dffc9334c02cce9b5a5a5d6b3ea5868325f
Fabio Carosi <s1089298@studenti.univpm.it>
2021-01-27 15:39:47 +0100

    v. 0.0.0.13

--- TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/Venue.java
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/Venue.java
@@ -13,6 +13,13 @@ public class Venue {
 	/**
 	 * 
 	 */
+	private String name;
+	private String line1;
+	private String stateCode;
+	private String stateName;
+	private String cityName;
+	private String countryName;
+	private String countryCode;
 	public Venue() {
 		// TODO Auto-generated constructor stub
 	}
