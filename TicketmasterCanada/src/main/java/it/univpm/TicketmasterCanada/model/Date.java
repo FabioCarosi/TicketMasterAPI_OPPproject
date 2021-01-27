0d52caae57a546af9fa4f273aba2c903e9fc08a7
Fabio Carosi <s1089298@studenti.univpm.it>
2021-01-27 18:10:52 +0100

    v. 0.0.1.3

---
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/Date.java
@@ -0,0 +1,76 @@
+
+package it.univpm.TicketmasterCanada.model;
+
+
+/**
+ * @author Mattia Girolami
+ * @author Fabio Carosi
+ */
+
+public class Date {
+	
+	private String data;
+	private String orario;
+	
+	
+	/**
+	 * @param data
+	 * @param orario
+	 */
+	public Date(String data, String orario) {
+		super();
+		this.data = data;
+		this.orario = orario;
+	}
+
+
+	public String getData() {
+		return data;
+	}
+
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+
+	public String getOrario() {
+		return orario;
+	}
+
+
+	public void setOrario(String orario) {
+		this.orario = orario;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return data +  "  " + orario;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date date = (Date) obj;
+		if (data == null) {
+			if (date.data != null)
+				return false;
+		} else if (!data.equals(date.data))
+			return false;
+		if (orario == null) {
+			if (date.orario != null)
+				return false;
+		} else if (!orario.equals(date.orario))
+			return false;
+		
+		return true;
+	}
+
+}
