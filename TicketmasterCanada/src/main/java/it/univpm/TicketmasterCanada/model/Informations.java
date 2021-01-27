ee9c961ea53fbd1b141876be8fb89cbdb25291b0
Fabio Carosi <s1089298@studenti.univpm.it>
2021-01-27 12:32:33 +0100

    v. 0.0.0.12

---
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/Informations.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.univpm.TicketmasterCanada.model;
+
+/**
+ * @author Fabio Carosi
+ * @author Mattia Girolami
+ *
+ */
+public class Informations {
+
+	/**
+	 * 
+	 */
+	public Informations() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
