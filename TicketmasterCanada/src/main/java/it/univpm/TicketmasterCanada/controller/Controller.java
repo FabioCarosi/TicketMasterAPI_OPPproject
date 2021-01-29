df5eb5591f2b36e29e4436f2a2856dcdfff3c853
Mattia Girolami <s1087740@studenti.univpm.it>
2021-01-29 17:08:05 +0100

    v. 0.0.2.5 [Controller class definition]

---
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/controller/Controller.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package it.univpm.TicketmasterCanada.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONObject;
+
+import it.univpm.TicketmasterCanada.model.*;
+
+
+/**
+ * @author Mattia Girolami
+ * @author Fabio Carosi
+ */
+
+@RestController
+
+public class Controller {
+	
+	@Autowired
+	
+	@GetMapping(value = "/events")
+	public ResponseEntity<Object> getStateEvents(@RequestParam String stateName){
+		Venue venue = service.getStateEventsfromApi(stateName);
+		
+		JSONObject obj = new JSONObject();
+		
+		return new ResponseEntity<> (obj.toString(), HttpStatus.OK);
+	}
+}
