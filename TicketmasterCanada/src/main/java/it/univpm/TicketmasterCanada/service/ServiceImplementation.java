5e6cc3239cea1fa0c654ed57b940dfececba7b7e
Mattia Girolami <s1087740@studenti.univpm.it>
2021-01-29 16:22:43 +0100

    v. 0.0.2.4 [Defining ServiceImplementation class]

---
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/service/ServiceImplementation.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package it.univpm.TicketmasterCanada.service;
+
+/**
+ * @author Mattia Girolami
+ * @author Fabio Carosi
+ */
+public class ServiceImplementation {
+
+	/**
+	 * api_key
+	 */
+	private String api_key = "Ccg2GNVOGvUUXJeAPtSSAEQZjxbFN75B";
+}
