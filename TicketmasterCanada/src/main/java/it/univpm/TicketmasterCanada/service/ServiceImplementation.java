3e35e03c11ba4c1fcad395d1dd221d3f0dc9cc83
Fabio Carosi <s1089298@studenti.univpm.it>
2021-02-02 17:26:08 +0100

    v. 0.0.2.30 [Creation of VectorFiller method in ServiceImplementation]

--- TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/service/ServiceImplementation.java
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/service/ServiceImplementation.java
@@ -136,9 +136,66 @@ public class ServiceImplementation implements it.univpm.TicketmasterCanada.servi
 		return venue;
 }
 	
-	
-	public Venue getChosenCountryEventsfromApi(String countryCode) {
+	public Vector<Event> VectorFiller(String code)
+	{
 		
+		JSONObject Object = 
+		JSONArray eventsArray = countryAPIObj.getJSONArray("events");
+		JSONObject stack;
+		Vector<Event> FullVector = new Vector<Event>(eventsArray.length());
+	
+		
+		
+		for (int i = 0; i<eventsArray.length(); i++) {
+			
+			Event event = new Event();              		//creo un nuovo oggetto di tipo event
+			stack = eventsArray.getJSONObject(i); 			//i-esimo blocco nel vettore degli eventi
+			event.setName(stack.getString("name")); 		//setto il nome dell'evento con la stringa che corrisponde a "name"
+			event.setId(stack.getString("id"));  			//setto l'id dell'evento con la stringa che corrisponde a "id"
+			event.setUrl(stack.getString("url"));			//setto l'url dell'evento con la stringa che corrisponde a "url"
+			event.setInfo(stack.getString("info"));			//setto l'inof dell'evento con la stringa che corrisponde a "info"
+			
+	
+			Date data = new Date(); 													//creo un nuovo oggetto di tipo data
+			JSONArray datesArray = stack.getJSONArray("dates"); 						//creo un JSONArray che corrisponde all'array "dates"
+			JSONObject datesObject = datesArray.getJSONObject(0);						//creo un JSONObject a partire dal dataArray precedente
+			JSONArray startDateArray = datesObject.getJSONArray("start");				//creo un JSONArray che corrisponde all'array "start"
+			JSONObject startDateObject = startDateArray.getJSONObject(0);				//creo un JSONObject a partire dal startDateArray precedente
+			data.setData(startDateObject.getString("localDate")); 						//setto la data della data con la stringa che corrisponde a "localDate"
+			data.setOrario(startDateObject.getString("localTime")); 					//setto l'orario della data con la stringa che corrisponde a "localTime"
+			event.setDate(data); 														//setto la data dell'evento con l'oggetto di tipo data appena creato
+			
+			Informations information = new Informations(); 								//creo un ogetto di tipo informations
+			JSONArray informationsArray = stack.getJSONArray("priceRanges");			//creo un JSONArray che corrisponde all'array "priceRanges"
+			JSONObject informationsObject = informationsArray.getJSONObject(0); 		//creo un JSONObject a partire dal informationsArray precedente
+			information.setCurrency(informationsObject.getString("currency")); 			//setto la valuta con la stringa che corrisponde a "currency"
+			information.setMaxPrice(informationsObject.getDouble("max"));				//setto il prezzo massimo con la stringa che corrisponde a "max"
+			information.setMinPrice(informationsObject.getDouble("min"));				//setto il prezzo minimo con la stringa che corrisponde a "min"
+			event.setInformations(information); 										//setto le informations dell'evento con l'oggetto di tipo infromations appena creato
+			
+			Genre genre = new Genre(); 													//creo un ogetto di tipo Genre
+			JSONArray classificationsArray = stack.getJSONArray("classifications");		//creo un JSONArray che corrisponde all'array "classifications"
+			JSONObject classificationsObject = classificationsArray.getJSONObject(0);	//creo un JSONObject a partire dal informationsArray precedente
+			JSONArray segmentsArray = classificationsObject.getJSONArray("segment");	//creo un JSONArray che corrisponde all'array "segment"
+			JSONObject segmentsObject = segmentsArray.getJSONObject(0); 				//creo un JSONObject a partire dal segmentArray precedente
+			genre.setSegmentName(segmentsObject.getString("name")); 					//setto il nome del tipologia con la stringa che corrisponde a "name"
+			JSONArray genresArray = classificationsObject.getJSONArray("genre"); 		//creo un JSONArray che corrisponde all'array "genre"
+			JSONObject genresObject = genresArray.getJSONObject(0);						//creo un JSONObject a partire dal genresArray precedente
+			genre.setGenreName(genresObject.getString("name")); 						//setto il nome del genre con la stringa che corrisponde a "name"
+			JSONArray subGenresArray = classificationsObject.getJSONArray("subGenre");	//creo un JSONArray che corrisponde all'array "subGenre"
+			JSONObject subGenresObject = subGenresArray.getJSONObject(0);				//creo un JSONObject a partire dal subGenreArray precedente
+			genre.setSubGenreName(subGenresObject.getString("name")); 					//setto il nome del sottogenere con la stringa che corrisponde a "name"
+			event.setGenre(genre); 														//setto il genere dell'evento con l'oggetto genre appena creato
+			vector.add(event); 
+		}
+		
+		venue.setVectorEvent(vector);
+		
+		return FullVector;
+		
+	}
+		
+	public Venue getChosenCountryEventsfromApi(String countryCode) {
 		JSONObject countryAPIObj = getCountryEvents(countryCode);
 		
 		Country country = new Country(countryCode);
