56d5d25c2603fae950db0ed7984575f5f7deb5b0
Mattia Girolami <s1087740@studenti.univpm.it>
2021-02-02 11:30:22 +0100

    v. 0.0.2.23 [Implementing State & Market classes]

--- TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/Market.java
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/Market.java
@@ -9,7 +9,109 @@ package it.univpm.TicketmasterCanada.model;
  */
 public class Market {
 	
-	private int marketID; //rappresenta l'id di un ampio raggruppamento geografico
-	private String marketName; //rappresenta il nome di un ampio raggruppamento geografico
-
+	private int marketID; 		//rappresenta l'id di un ampio raggruppamento geografico
+	private String marketName; 	//rappresenta il nome di un ampio raggruppamento geografico
+	
+	/**
+	 * Metodo che restituisce il:
+	 * @return the marketID
+	 */
+	public int getMarketID() {
+		return marketID;
+	}
+	
+	/**
+	 * Metodo che setta il:
+	 * @param marketID the marketID to set
+	 */
+	public void setMarketID(int marketID) {
+		this.marketID = marketID;
+	}
+	
+	/**
+	 * Metodo che restituisce il:
+	 * @return the marketName
+	 */
+	public String getMarketName() {
+		return marketName;
+	}
+	
+	/**
+	 * Metodo che setta il:
+	 * @param marketName the marketName to set
+	 */
+	public void setMarketName(String marketName) {
+		this.marketName = marketName;
+	}
+	
+	/**
+	 * Costruttore con parametri:
+	 * @param marketID
+	 * @param marketName
+	 */
+	public Market(int marketID, String marketName) {
+		super();
+		this.marketID = marketID;
+		this.marketName = marketName;
+	}
+	
+	/**
+	 * Costruttore con parametro:
+	 * @param marketID
+	 */
+	public Market(int marketID) {
+		super();
+		this.marketID = marketID;
+		this.marketName = null;
+	}
+	
+	/**
+	 * Costruttore con parametro:
+	 * @param marketName
+	 */
+	public Market(String marketName) {
+		super();
+		this.marketID = 0;
+		this.marketName = marketName;
+	}
+	
+	/**
+	 * Costruttore vuoto.
+	 */
+	public Market() {
+		super();
+		this.marketID = 0;
+		this.marketName = null;
+	}
+	
+	/**
+	 * Override del metodo toString.
+	 */
+	@Override
+	public String toString() {
+		return "marketID=" + marketID + ", marketName=" + marketName;
+	}
+	
+	/**
+	 * Override del metodo equals.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Market other = (Market) obj;
+		if (marketID != other.marketID)
+			return false;
+		if (marketName == null) {
+			if (other.marketName != null)
+				return false;
+		} else if (!marketName.equals(other.marketName))
+			return false;
+		return true;
+	}
+	
 }
--- TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/State.java
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/State.java
@@ -9,8 +9,103 @@ package it.univpm.TicketmasterCanada.model;
  */
 public class State {
 	
-	private String stateCode; //rappresenta l'abbreviazione del territorio (es. AB, MB...)
-	private String stateName; //rappresenta il nome del territorio (es. Alberta, Manitoba...)
+	private String stateCode; 	//rappresenta l'abbreviazione del territorio (es. AB, MB...)
+	private String stateName; 	//rappresenta il nome del territorio (es. Alberta, Manitoba...)
+	
+	/**
+	 * Metodo che restituisce il:
+	 * @return the stateCode
+	 */
+	public String getStateCode() {
+		return stateCode;
+	}
+	
+	/**
+	 * Metodo che setta il:
+	 * @param stateCode the stateCode to set
+	 */
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+	
+	/**
+	 * Metodo che restituisce il:
+	 * @return the stateName
+	 */
+	public String getStateName() {
+		return stateName;
+	}
+	
+	/**
+	 * Metodo che setta il:
+	 * @param stateName the stateName to set
+	 */
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+	
+	/**
+	 * Costruttore con parametri:
+	 * @param stateCode
+	 * @param stateName
+	 */
+	public State(String stateCode, String stateName) {
+		super();
+		this.stateCode = stateCode;
+		this.stateName = stateName;
+	}
+	
+	/**
+	 * Costruttore con parametro:
+	 * @param stateCode
+	 */
+	public State(String stateCode) {
+		super();
+		this.stateCode = stateCode;
+		this.stateName = null;
+	}
 	
 
+	/**
+	 * Costruttore vuoto.
+	 */
+	public State() {
+		super();
+		this.stateCode = null;
+		this.stateName = null;
+	}
+	
+	/**
+	 * Override del metodo toString.
+	 */
+	@Override
+	public String toString() {
+		return "State stateCode=" + stateCode + ", stateName=" + stateName;
+	}
+
+	/**
+	 * Override del metodo equals.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (stateCode == null) {
+			if (other.stateCode != null)
+				return false;
+		} else if (!stateCode.equals(other.stateCode))
+			return false;
+		if (stateName == null) {
+			if (other.stateName != null)
+				return false;
+		} else if (!stateName.equals(other.stateName))
+			return false;
+		return true;
+	}
+	
 }
