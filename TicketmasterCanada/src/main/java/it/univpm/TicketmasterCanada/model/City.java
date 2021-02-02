665de96e5df6ef84e12a90ab184a9bb9e4917b77
Fabio Carosi <s1089298@studenti.univpm.it>
2021-02-02 11:51:26 +0100

    v. 0.0.2.24b [Update comments]

--- TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/City.java
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/model/City.java
@@ -4,7 +4,8 @@
 package it.univpm.TicketmasterCanada.model;
 
 /**
- * @author caros
+ * @author Fabio Carosi
+ * @author Mattia Girolami
  *
  */
 public class City {
@@ -12,70 +13,86 @@ public class City {
 	private String venueName; //rappresenta il nome della località
 	private String address;   //rappresenta l'indirizzo dove si svolge l'evento
 	private String cityName;  //rappresenta il nome della città (es. Edmonton, Winnipeg)
+	
 	/**
+	 * Metodo che restituisce il nome del posto dell'evento
 	 * @return the venueName
 	 */
 	public String getVenueName() {
 		return venueName;
 	}
 	/**
+	 * Setta il nome del posto dell'evento
 	 * @param venueName the venueName to set
 	 */
 	public void setVenueName(String venueName) {
 		this.venueName = venueName;
 	}
 	/**
+	 * Metodo che restituisce l'indirizzo dell'evento
 	 * @return the address
 	 */
 	public String getAddress() {
 		return address;
 	}
 	/**
+	 * Setta l'indirizzo dell'evento
 	 * @param address the address to set
 	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
 	/**
+	 * Metodo che restituisce il nome della città dell'evento
 	 * @return the cityName
 	 */
 	public String getCityName() {
 		return cityName;
 	}
 	/**
+	 * Setta il nome della citta' dell'evento
 	 * @param cityName the cityName to set
 	 */
 	public void setCityName(String cityName) {
 		this.cityName = cityName;
 	}
 	/**
-	 * 
+	 * Costruttore vuoto
 	 */
 	public City() {
 		super();
+		this.venueName = null;
+		this.address = null;
+		this.cityName = null;
 	}
 	/**
+	 * Costruttore con parametro il nome della citta'
 	 * @param cityName
 	 */
 	public City(String cityName) {
 		super();
 		this.cityName = cityName;
+		this.venueName = null;
+		this.address = null;
 	}
 	/**
+	 * Cosrtuttore con parametro nome della citta' e indirizzo
 	 * @param address
 	 * @param cityName
 	 */
-	public City(String address, String cityName) {
+	public City(String cityName, String address) {
 		super();
 		this.address = address;
 		this.cityName = cityName;
+		this.venueName = null;
 	}
 	/**
+	 * costruttore con tutti e tre i parametri
 	 * @param venueName
 	 * @param address
 	 * @param cityName
 	 */
-	public City(String venueName, String address, String cityName) {
+	public City(String cityName, String address, String venueName) {
 		super();
 		this.venueName = venueName;
 		this.address = address;
@@ -111,7 +128,7 @@ public class City {
 	}
 	
 	
-
+	//Ritorna una stringa con tutti gli attributi
 	@Override
 	public String toString() {
 		return "City [venueName=" + venueName + ", address=" + address + ", cityName=" + cityName + "]";
