ca34a578a91d532cc8e93be0daf598697384ce5c
Mattia Girolami <s1087740@studenti.univpm.it>
2021-02-03 17:56:48 +0100

    v. 0.0.3.6 [JsonCreator and ServiceImplementation Update]

--- TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/service/JsonCreator.java
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/service/JsonCreator.java
@@ -32,7 +32,7 @@ public class JsonCreator {
 		for(int i=0 ; i < (eventVector.getVector()).size(); i++) {
 			
 			object.put("id", (eventVector.getVector()).get(i).getId());
-			object.put("info", (eventVector.getVector()).get(i).getInfo());
+			//object.put("info", (eventVector.getVector()).get(i).getInfo());
 			object.put("name", (eventVector.getVector()).get(i).getName());
 			object.put("url", (eventVector.getVector()).get(i).getUrl());
 			
@@ -42,24 +42,24 @@ public class JsonCreator {
 			object.put("stateCode", (eventVector.getVector()).get(i).getVenue().getState().getStateCode());
 			object.put("stateName", (eventVector.getVector()).get(i).getVenue().getState().getStateName());
 			
-			object.put("marketName", (eventVector.getVector()).get(i).getVenue().getMarket().getMarketName());
-			object.put("marketID", (eventVector.getVector()).get(i).getVenue().getMarket().getMarketID());
+			//object.put("marketName", (eventVector.getVector()).get(i).getVenue().getMarket().getMarketName());
+			//object.put("marketID", (eventVector.getVector()).get(i).getVenue().getMarket().getMarketID());
 			
 			object.put("cityName", (eventVector.getVector()).get(i).getVenue().getCity().getCityName());
 			object.put("address", (eventVector.getVector()).get(i).getVenue().getCity().getAddress());
 			object.put("venueName", (eventVector.getVector()).get(i).getVenue().getCity().getVenueName());
 			
-			object.put("data", (eventVector.getVector()).get(i).getDate().getData());
-			object.put("orario", (eventVector.getVector()).get(i).getDate().getOrario());
+			//object.put("data", (eventVector.getVector()).get(i).getDate().getData());
+			//object.put("orario", (eventVector.getVector()).get(i).getDate().getOrario());
 			
 			object.put("segment", (eventVector.getVector()).get(i).getGenre().getSegmentName());
 			object.put("genre", (eventVector.getVector()).get(i).getGenre().getGenreName());
 			object.put("subgenre", (eventVector.getVector()).get(i).getGenre().getSubGenreName());
 			
-			object.put("currency", (eventVector.getVector()).get(i).getInformations().getCurrency());
-			object.put("maxPrice", (eventVector.getVector()).get(i).getInformations().getMaxPrice());
-			object.put("minPrice", (eventVector.getVector()).get(i).getInformations().getMinPrice());
-			object.put("source", (eventVector.getVector()).get(i).getInformations().getSource());
+			//object.put("currency", (eventVector.getVector()).get(i).getInformations().getCurrency());
+			//object.put("maxPrice", (eventVector.getVector()).get(i).getInformations().getMaxPrice());
+			//object.put("minPrice", (eventVector.getVector()).get(i).getInformations().getMinPrice());
+			//object.put("source", (eventVector.getVector()).get(i).getInformations().getSource());
 			
 			arr.put(object);
 		}
