035ec6eadc83540ef14ce094c0c21551df127104
Fabio Carosi <s1089298@studenti.univpm.it>
2021-02-05 16:07:08 +0100

    v. 0.0.4.7 [Creation of new filters class]

---
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/filters/FilterMarketId.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package it.univpm.TicketmasterCanada.filters;
+
+/**
+ * @author Fabio Carosi
+ * @author Mattia Girolami
+ */
+public class FilterMarketId {
+
+	/**
+	 * 
+	 */
+	public FilterMarketId() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
