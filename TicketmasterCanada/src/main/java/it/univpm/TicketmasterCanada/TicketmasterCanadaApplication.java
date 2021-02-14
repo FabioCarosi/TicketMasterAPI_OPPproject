60af65b4094841b8ff3eee17697d516d0894f270
Mattia Girolami <s1087740@studenti.univpm.it>
2021-02-14 17:02:34 +0100

    v. 0.0.5.18 [Updated main]

--- TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/TicketmasterCanadaApplication.java
+++ TicketmasterCanada/src/main/java/it/univpm/TicketmasterCanada/TicketmasterCanadaApplication.java
@@ -3,12 +3,22 @@ package it.univpm.TicketmasterCanada;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.univpm.TicketmasterCanada.files.*;
 
+/** 
+ * 
+ * @author Mattia Girolami
+ * @author Fabio Carosi
+ * 
+ */
 @SpringBootApplication
 public class TicketmasterCanadaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketmasterCanadaApplication.class, args);
+		
+		Booster save = new Booster();
+		save.salvaBooster();
 	}
 
 }
